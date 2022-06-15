9a45ccce563d0d66ebe3492caabec286e97c8694
Mohammad Areeb <areebchamp0786@gmail.com>
2022-06-15 15:17:46 +0530

    added runtime,requirements and Procfile

--- expense/settings.py
+++ expense/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ei0x6)s$%2q!0=$s8h2o*r37afwkyc22tc2+(_&s4+sxsof-^e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['track-my-expenses99.herokuapp.com','127.0.0.1']
 
 
 # Application definition
