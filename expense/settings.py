ac4a2f1bc9aba01a87f291d8742ed0a56caf1470
Mohammad Areeb <areebchamp0786@gmail.com>
2022-06-15 20:19:53 +0530

    modified settings.py and procfile

--- expense/settings.py
+++ expense/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ei0x6)s$%2q!0=$s8h2o*r37afwkyc22tc2+(_&s4+sxsof-^e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['track-my-expenses99.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,9 +116,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATIC_URL = 'static/'
+STATIC_ROOT = 'staticfiles'
 
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIR = {
+    os.path.join(BASE_DIR,'public/static')
+}
+
+MEDIA_ROOT=os.path.join(BASE_DIR,'public/static')
+MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
